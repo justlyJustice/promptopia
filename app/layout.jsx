b7d8f9d1255f56374648ba0ea21d1d27921624cb
Justice Johnson <justlyjohn198@gmail.com>
2023-07-27 17:28:10 +0100

    Implementation -> Create prompt page and API route

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,25 +6,21 @@ import Provider from "@components/Provider";
 export const metadata = {
   title: "Promptopia",
   description: "Discover and share AI Prompts",
-  openGraph: {
-    title: "Promptopia",
-    description: "Discover and share AI Prompts",
-    images: "/assets/logo.svg",
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
